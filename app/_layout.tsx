4c551c23a5f066154a07a3e43ff4d33c66c70f27
Mooshiiee <mooshcor@outlook.com>
2024-12-19 16:32:41 -0500

    first commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -29,8 +29,20 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#133434',
+          },
+          headerTintColor: '#19ae4b',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+      >
+        <Stack.Screen 
+          name="(tabs)" 
+        />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
